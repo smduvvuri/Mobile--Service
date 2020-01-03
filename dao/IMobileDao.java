4da95670266b8123c67bc39c4b1874ae933785ac
smduvvuri <shreemaduvvuri@gmail.com>
2020-01-03 00:29:54 -0800

    com.deloitte.telecom

---
+++ dao/IMobileDao.java
@@ -0,0 +1,23 @@
+package mobile.dao;
+import java.util.Map;
+import java.util.Set;
+
+import mobile.entities.Mobile;
+
+
+public interface IMobileDao {
+
+
+	    Mobile findBymobno(String mobileno);
+
+	    Set<Mobile> allMobiles();
+
+		void addMobile(Mobile w);
+
+		Mobile rechargeAmount(String str1, String name, double amount);
+
+		Map<String,Mobile> getStore();
+
+		
+	}
+
---
+++ service/IMobileService.java
@@ -0,0 +1,21 @@
+package mobile.service;
+	
+import java.util.Set;
+
+import mobile.dao.IMobileDao;
+import mobile.entities.Mobile;
+public interface IMobileService {
+		void addMobile(Mobile w);
+
+		Mobile findBymobno(String name);
+
+		Set<Mobile> allMobiles();
+
+		Mobile rechargeAmount(String str1, String name, double amount);
+
+		IMobileDao getDao();
+
+
+		
+	}
+
